88a22c4ba5a0cc63fb4a581237f9c09c47f538b2
Ivan Dronek <nomi29251@gmail.com>
2022-11-02 10:27:55 +0200

    Laboratory number 4 C sharp

--- Lab_4_C_sharp/Program.cs
+++ Lab_4_C_sharp/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_4_C_sharp
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        Circle circle1 = new Circle(5);
+        Circle circle2 = new Circle(25);
+
+        Console.WriteLine("Площа кола:" + circle1.Area());
+        Console.WriteLine("Площа кола:" + circle2.Area());
+        Console.WriteLine("Периметр кола:" + circle1.Perimeter());
+        Console.WriteLine("Периметр кола:" + circle2.Perimeter());
+
+        Square square1 = new Square(5);
+        Square square2 = new Square(25);
+
+        Console.WriteLine("Площа квадрату:" + square1.Area());
+        Console.WriteLine("Площа квадрату:" + square2.Area());
+        Console.WriteLine("Периметр квадрвту:" + square1.Perimeter());
+        Console.WriteLine("Периметр квадрвту:" + square2.Perimeter());
+
+
+
+
+
+
+        Console.ReadKey();
     }
 }
